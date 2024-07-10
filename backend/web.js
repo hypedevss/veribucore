e8b5a75a476f79adf0d424d8514fa7328e5f8a60
jotkauser <aleksander.dajnowicz@gmail.com>
2024-07-10 21:28:32 +0200

    aasasaas

--- backend/web.js
+++ backend/web.js
@@ -42,7 +42,7 @@ app.get(`/oauth2/discord`, async (req, res) => {
 	if (veribuAllat.includes(ip)) return res.sendFile('/frontend/almostthere/blocked.html', sopt)
 	if (code) {
 		const params = new URLSearchParams({
-			client_id: discord.client_id,
+			client_id: discord.clientId,
 			client_secret: oauth.discord.client_secret,
 			code: code,
 			grant_type: 'authorization_code',
