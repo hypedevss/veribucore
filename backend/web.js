3fb4e40cf89e09d7070e731092fa0bbf2ea5884a
jotkauser <aleksander.dajnowicz@gmail.com>
2024-07-10 21:28:55 +0200

    aaa

--- backend/web.js
+++ backend/web.js
@@ -85,7 +85,7 @@ app.get(`/oauth2/discord`, async (req, res) => {
 			// now redirect to the TikTok OAuth2 page
 			res.sendFile('frontend/almostthere/index.html', sopt)
 		} catch (error) {
-			console.error(error.response.data.client_id)
+			console.error(error.response.data)
 			res.status(500).send("Code expired.")
 		}
 	}
